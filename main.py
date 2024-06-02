3fae7297f9ad5d216aca535d076c72ad6344babc
A.srkr <143035320+aritrasa45@users.noreply.github.com>
2024-06-03 02:11:25 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+# instagram :- @0aritrasa1
+import string
+import time
+from colorama import *
+
+init()
+
+def color():
+	print(Style.RESET_ALL)
+
+def exit():
+	time.sleep(1)
+	sys.exit()
+
+def generate_password(length=12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+
+print(f"{Fore.CYAN}Press X to exit")
+color()
+time.sleep(0.5)
+
+
+while True:
+	pass_len = int(input(f"\n{Fore.RED}Enter the length of pass  :   "))
+	if pass_len == 'x':
+		exit()
+	else:
+		pass	
+	color()
+	gen_pass =generate_password(pass_len)
+	print("\n",Fore.GREEN+gen_pass)
